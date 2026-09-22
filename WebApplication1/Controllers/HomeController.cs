89997ba40e3c0b7d4d8da42c01fc8aec6a209595
Full Stack Developer <75879894+Vishal-Git-Repositories@users.noreply.github.com>
2026-09-22 17:56:07 +0530

    Update HomeController.cs
    demo6

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-//this is comment demo 5
+//this is comment demo 6
         public Home1Controller(ILogger<HomeController> logger)
         {
             _logger = logger;
